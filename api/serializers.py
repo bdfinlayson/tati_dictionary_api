9610e1f36ed258a32e8a8c991abb1ee3df46e35c
Bryan Finlayson <bdfinlayson@gmail.com>
2021-05-14 20:10:59 -0500

    add grammatical category

--- api/serializers.py
+++ api/serializers.py
@@ -8,15 +8,18 @@ class EntryEquivalentSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['id', 'language', 'position', 'word', 'definition', 'entry_id']
 
 
+class GrammaticalCategorySerializer(serializers.HyperlinkedModelSerializer):
+  class Meta:
+    model = GrammaticalCategory
+    fields = ['id', 'name']
+
+
 class DictionaryEntrySerializer(serializers.HyperlinkedModelSerializer):
     entry_equivalents = EntryEquivalentSerializer(many=True)
+    grammatical_category = GrammaticalCategorySerializer(many=False)
+
     class Meta:
         model = DictionaryEntry
-        fields = ['id', 'tati_word', 'ipa', 'createdAt', 'updatedAt', 'entry_equivalents']
+        fields = ['id', 'tati_word', 'ipa', 'createdAt', 'updatedAt', 'grammatical_category', 'entry_equivalents']
 
-
-class GrammaticalCategorySerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = GrammaticalCategory
-        fields = ['id', 'name']
 
